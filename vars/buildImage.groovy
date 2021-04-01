3dacdf2895d8080f86de6d0927354606dbdc7903
armughanahmed <armughan.ahmed@yahoo.com>
2021-04-01 12:25:30 +0500

    added nodejs wrapper

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,7 +3,6 @@
 import com.example.Docker
 
 def call(String packageJSON) {
-    // sh "echo ${packageJSON}"
-    sh 'echo $(grep version package.json | awk -F \" '{print $4}')'
+    sh "echo ${packageJSON}"
     // return new Docker(this).buildDockerImage(packageJSON)
 }
