496601249be708bf28c7dce62840e7646ee27bec
armughanahmed <armughan.ahmed@yahoo.com>
2021-04-02 15:46:29 +0500

    changes in checkout

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -19,7 +19,7 @@ class Docker implements Serializable {
     def deploy(String commitId) {
         def shellCmd = "bash ./server-cmds.sh ${script.env.IMAGE_NAME}:${commitId}-${script.BUILD_NUMBER}"
 
-        script.sshagent(['ec2-server-key']) {
+        script.sshagent(['ec2-user-key']) {
             script.sh "scp server-cmds.sh ${script.env.EC2_IP}:/home/ec2-user"
             script.sh "ssh -o StrictHostKeyChecking=no ${script.env.EC2_IP} ${shellCmd}"
         }
